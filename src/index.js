ee9218ade05a1bb91ac58b02eec120e8faedea35
Logan Angellotti <loganangellotti@aliasappss-iMac.local>
2019-09-30 11:04:28 -0400

    working get

--- src/index.js
+++ src/index.js
@@ -10,3 +10,49 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+class GetArticle extends React.Component {
+
+    handleClick = () => {
+        alert('test');
+    }
+
+
+    state = {
+        title: 'loading...'
+    }
+
+
+    componentDidMount() {
+        fetch('https://en.wikipedia.org/w/api.php?action=query&format=json&list=random&rnlimit=1&rnnamespace=0&origin=*')
+        .then(res => res.json())
+        .then(
+            (result) => {
+                console.log(result.query.random[0].title);
+                this.setState({
+                    title: result.query.random[0].title
+                });
+            }
+        )
+        .catch(console.log)
+
+       // fetch('https://en.wikipedia.org/w/api.php?action=parse&format=json&page=' + this.state.title)
+    }
+
+    //https://en.wikipedia.org/w/api.php?action=parse&format=json&page='title'
+    render() {
+        return (
+            <p>
+                {'https://en.wikipedia.org/w/api.php?action=parse&format=json&page=' + this.state.title}
+            </p>
+        );
+    }
+}
+
+
+
+ReactDOM.render(
+    <GetArticle />,
+    document.getElementById('root')
+);
